cf353f6da764dae0d7e49ec7b0b808a8cf5d05e4
Danil2537 <dedov7347@gmail.com>
2025-04-24 23:26:06 +0300

    Changed ORM from Drizzle to Prisma. Tried Next-Auth but failed due to library bug. Used Clerk Auth instead. The login should be a button in profile, as clerk takes user to it's own page.

---
+++ src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import { type AppType } from "next/app";
+import { Geist } from "next/font/google";
+
+import { api } from "~/utils/api";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs';
+import '~/styles/globals.css';
+const geist = Geist({
+  subsets: ["latin"],
+});
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <div className={geist.className}>
+       <ClerkProvider>
+    <Component {...pageProps} />
+  </ClerkProvider>
+    </div>
+  );
+};
+
+export default api.withTRPC(MyApp);
