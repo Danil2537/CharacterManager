b899aef8fb3f1e51a8411a1bcdb48d4844f2292c
Danil2537 <dedov7347@gmail.com>
2025-05-19 13:17:39 +0300

    fixing linting errors

--- src/server/api/routers/creation.ts
+++ src/server/api/routers/creation.ts
@@ -1,23 +1,42 @@
-import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "../trpc";
-import { getAuth } from "@clerk/nextjs/server";
-import type { IncomingMessage } from "http";
 
 export const creationRouter = createTRPCRouter({
-    getAllClasses: publicProcedure.query(async ({ ctx }) => { 
-        const classes = await ctx.prisma.class.findMany();
-        console.log("Fetched classes", JSON.stringify(classes, null, 2));
-        return classes;
+  getAllClasses: publicProcedure.query(async ({ ctx }) => { 
+    const classes = await ctx.prisma.class.findMany();
+    console.log("Fetched classes", JSON.stringify(classes, null, 2));
+    return classes;
+  }),
 
-    }),
-    getAllSpecies: publicProcedure.query(async ({ ctx }) => { }),
-    getAllBackgrounds: publicProcedure.query(async ({ ctx }) => { }),
-    saveClass: publicProcedure.query(async ({ ctx }) => { }),
-    saveSpecies: publicProcedure.query(async ({ ctx }) => { }),
-    savebackground: publicProcedure.query(async ({ctx})=>{}),
-    getAvailableEquipment: publicProcedure.query(async ({})=>{}),
-    saveEquipment: publicProcedure.query(async ({})=>{}),
-    //TODO: roll ability scores on client
-    saveAbilityScores: publicProcedure.query(async ({})=>{}),
+  getAllSpecies: publicProcedure.query(async (_ctx) => {
+    // TODO: Implement getAllSpecies
+  }),
 
+  getAllBackgrounds: publicProcedure.query(async (_ctx) => {
+    // TODO: Implement getAllBackgrounds
+  }),
+
+  saveClass: publicProcedure.query(async (_ctx) => {
+    // TODO: Implement saveClass
+  }),
+
+  saveSpecies: publicProcedure.query(async (_ctx) => {
+    // TODO: Implement saveSpecies
+  }),
+
+  savebackground: publicProcedure.query(async (_ctx) => {
+    // TODO: Implement savebackground
+  }),
+
+  getAvailableEquipment: publicProcedure.query(async () => {
+    // TODO: Implement getAvailableEquipment
+  }),
+
+  saveEquipment: publicProcedure.query(async () => {
+    // TODO: Implement saveEquipment
+  }),
+
+  // TODO: roll ability scores on client
+  saveAbilityScores: publicProcedure.query(async () => {
+    // TODO: Implement saveAbilityScores
+  }),
 });
