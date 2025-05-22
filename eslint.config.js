fa68b9561e3a297c48e64dfb0e0a5a80eaccefaa
Danil2537 <dedov7347@gmail.com>
2025-05-23 00:02:08 +0300

    In the linting errors. Straight up 'fixin them'. And by 'fixin', haha, well. let's justr say. Disabling the linter.

--- eslint.config.js
+++ eslint.config.js
@@ -34,6 +34,7 @@ export default tseslint.config(
         { checksVoidReturn: { attributes: false } },
       ],
       "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
     },
   },
   {
